0a9e66e6ed0341f0e29806196c4fbce86a0579c4
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2024-08-28 15:25:56 -0400

    adds navigation blockout walls

---
+++ Core/MapObjects/BlockoutWall.cs
@@ -0,0 +1,58 @@
+ ///////////////////////////////////////////////////////////////////////////////
+ // Project: BallentinIsland
+ //
+ //
+ // Created by Ethan Kisiel
+ // Created on Tue Aug 27 2024
+ //
+ //
+ //
+ // Copyright (c) 2024 Company
+ ///////////////////////////////////////////////////////////////////////////////
+
+using Godot;
+using System;
+
+
+namespace BallentinIsland.Core.MapObjects
+{
+[   Tool]
+    public partial class BlockoutWall : Node3D
+    {
+        // Called when the node enters the scene tree for the first time.
+
+        private Vector3 _scaleOverride;
+
+        [Export]
+        public Vector3 ScaleOverride { 
+            get => _scaleOverride;
+            set
+            {
+                _scaleOverride = value;
+                OnScaleOverrideChanged();
+            }
+        }
+
+        public NavigationObstacle3D NavObstacle { get; set; }
+        public MeshInstance3D Mesh { get; set; }
+
+        public override void _EnterTree()
+        {
+            NavObstacle = GetNode<NavigationObstacle3D>("NavObstacle");
+            Mesh = GetNode<MeshInstance3D>("Mesh");
+
+            OnScaleOverrideChanged();
+        }
+
+        // Called every frame. 'delta' is the elapsed time since the previous frame.
+
+        private void OnScaleOverrideChanged()
+        {
+            if (Mesh != null && NavObstacle != null)
+            {
+                Mesh.Scale = ScaleOverride;
+                NavObstacle.Scale = ScaleOverride / 1.5f;
+            }
+        }
+    }
+}
